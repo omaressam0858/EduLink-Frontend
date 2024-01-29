686d01a5ecd17b29ed598834f90d684bf9503bf3
Omar Essam <129010263+omaressam0858@users.noreply.github.com>
2024-01-30 01:41:32 +0200

    Update AddStudent.js

--- src/Students/AddStudent.js
+++ src/Students/AddStudent.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const API = "http://localhost:3000/api";
+const API = require('../api.config').API_URL;
 
 const StudentAdd = () => {
     const [groups, setGroups] = useState([]);
